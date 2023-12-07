9cbfe4a7fb8b4a158ca23d5a6c40b3c66f8838aa
MagicDev0428 <fullstackdever425@gmail.com>
2023-12-07 08:19:46 +0600

    Modified Multi file wrapper in AdamForm

--- investor-system/src/app/adam/adam-form.component.ts
+++ investor-system/src/app/adam/adam-form.component.ts
@@ -1,20 +1,48 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import {
+  Validators,
+  FormGroup,
+  NonNullableFormBuilder,
+  FormControl,
+} from "@angular/forms";
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-adam-form',
   templateUrl: './adam-form.component.html',
   styleUrls: ['./investorForm.scss', './adam-form.component.scss'],
 })
+
 export class AdamFormComponent {
 
   amount = '';
   textAlign = '';
+  files: any[] = [];
 
-  constructor() { }
+  protected adamForm: FormGroup;
+
+  constructor(private activatedRoute: ActivatedRoute, private formBuilder: FormBuilder) {
+
+   }
 
   ngOnInit(): void {
     this.amount = String.fromCharCode(3647);
     this.textAlign = 'right';
+    this.adamForm = this.formBuilder.group(
+      {
+        amount: new FormControl(""),
+        invest_from: new FormControl(),
+        invest_to: new FormControl(""),
+        trans_date: new FormControl(""),
+        investment: new FormControl(),
+        investor: new FormControl(),
+        trans_from: new FormControl(),
+        trans_to: new FormControl(),
+        trans_no: new FormControl(),
+        description: new FormControl(),
+        receiptImages: new FormControl()
+      });
   }
 
   onlyNumbers(event: KeyboardEvent) {
@@ -31,8 +59,7 @@ export class AdamFormComponent {
   }
 
   validation(event: any) {
-    let asciiCode = 3647; // ASCII code for Thai Baht symbol
-    let thb_character = String.fromCharCode(asciiCode);
+    let thb_character = String.fromCharCode(3647);
     let value = event.target.value;
     value = value.replace(/,/g, ''); // Remove existing commas
     value = value.replace(thb_character, ''); //Remove existing thb mark
@@ -40,6 +67,60 @@ export class AdamFormComponent {
     value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ','); // Add commas every three numbers
     value = value.replace(value, thb_character + ' ' + value);
     this.amount = value;
+  }
+
+  protected onSubmit(): void {
+
+  }
+
+  /**
+* on file drop handler
+*/
+  onFileDropped($event) {
+    this.prepareFilesList($event);
+  }
+  /**
+  * handle file from browsing
+  */
+  fileBrowseHandler(target) {
+    let files = target?.files;
+    this.prepareFilesList(files);
+  }
+
+  /**
+  * Convert Files list to normal array list
+  * @param files (Files List)
+  */
+  prepareFilesList(files: Array<any>) {
+    for (const item of files) {
+      this.files.push(item);
+    }
+    //this.addInvestorForm.get('passportImage').setValue(this.files);
+
+  }
+
+  /**
+   * format bytes
+   * @param bytes (File size in bytes)
+   * @param decimals (Decimals point)
+   */
+  formatBytes(bytes, decimals = 0) {
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
+    const k = 1024;
+    const dm = decimals <= 0 ? 0 : decimals || 2;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+
+  /**
+ * Delete file from files list
+ * @param index (File index)
+ */
+  deleteFile(index: number) {
+    this.files.splice(index, 1);
   }
 
 }
